ca482b418d5de3f7ff59d74f73696a38128955cf
CloneTwo7 <travisp0027@gmail.com>
2022-09-27 18:53:45 +0000

    simplified program

--- assignment4.c
+++ assignment4.c
@@ -13,9 +13,6 @@ description:
 #include <sys/wait.h>
 #include <stdio.h>
 #include <errno.h>
-
-#include <assert.h>
-
 /**********************************************************************
 ./assignment4 <arg1> : <arg2>
 
@@ -27,69 +24,53 @@ description:
 
 
 int main(int argc, char *argv[]){
-	int i, n;
+	int i;
 	int fd[2];
 	int rdr, wtr; 
-	char buff[255];
-	char *cmd_one[argc];
-	char *cmd_two[argc];
+	char **cmd_one;
+	char **cmd_two;
 	//Needs more extensive error checking
 	if(pipe (fd) < 0) {
-		exit(1);
+		perror("Pipe Broken: ");
 	}
 	rdr = fd[0]; wtr = fd[1];
 
-	/*I do not like how messy and blocky the following code is
-	 * but it does its job. It creates an array of strings for 
-	 * each command argument including their flags*/
 	int cmdFlag = 0;
-	int switchIndex = 0;
-	int cIndexOne = 0;
-	int cIndexTwo = 0;
+	cmd_one = &argv[1];
 	for(int i = 1; i < argc; i++) {
 		if(strcmp(argv[i], ":") == 0) {
+			argv[i] = '\0';
 			cmdFlag = 1;
-			switchIndex = i;
-		}
-		if( i > switchIndex && cmdFlag == 1) {
-			cmd_two[cIndexTwo] = calloc(1,strlen(argv[i])+1);
-			strcpy(cmd_two[cIndexTwo], argv[i]);
-			strcat(cmd_two[cIndexTwo], "\0");
-			cIndexTwo++;
-			cmd_two[cIndexTwo] = NULL;
-		} else if( cmdFlag != 1) {
-			cmd_one[cIndexOne] = calloc(1,strlen(argv[i])+1);
-			strcpy(cmd_one[cIndexOne], argv[i]);
-			strcat(cmd_one[cIndexOne], "\0");
-			cIndexOne++;
-			cmd_one[cIndexOne] = NULL;
+			cmd_two = &argv[i+1];
+			break;
 		}
 	}
 
-	/* Parent process handles the right side of the command
-	 * */
+	/* Parent process handles the right side of the command*/
 	if(fork()) {
+		if( cmdFlag != 1) {
+			wait(NULL);
+			exit(1);
+		}
 		close(wtr);
 		close(0); dup(rdr); close(rdr);
-		char *cmd = calloc(1, strlen(cmd_two[0]) + 6);
-		strcat(cmd, "/bin/");
-		strcat(cmd, cmd_two[0]);
-		strcat(cmd,"\0");
-		wait(NULL);
-		execvp(cmd, cmd_two);
+		if(execvp(cmd_two[0], cmd_two) == -1) {
+			exit(0);
+		}
 		exit(1);
-		
-	} else { //Child
-		close(rdr);
-		close(1); dup(wtr); close(wtr);
-		//allocate enough space for the cmd plus '/bin/' and null terminator
-		char *cmd = calloc(1, strlen(cmd_one[0]) + 6); 
-		strcat(cmd, "/bin/");
-		strcat(cmd, cmd_one[0]);
-		strcat(cmd, "\0");
-		execvp(cmd, cmd_one);
+	} 
+	/* Child process handles the left side of the command*/
+	else {
+		if (close(rdr) == -1) {
+			perror("Cannot close file descriptor");
+		}
+		if(cmdFlag == 1) {
+			close(1); dup(wtr); close(wtr);
+		}
+		if( execvp(cmd_one[0], cmd_one) == -1) {
+			exit(0);
+		}
 		exit(0);
 	}
-
 	return 0;
 }
